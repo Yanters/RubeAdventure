f13266e6c8a498f05dc2e94b84e0243fe722b3eb
Yanters <fajerbool@wp.pl>
2020-11-23 19:19:06 +0100

    Damega Zones and Enemies

---
+++ Assets/Scripts/DamageZone.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageZone : MonoBehaviour
+{
+    void OnTriggerStay2D(Collider2D collider)
+    {
+        RubyController controller = collider.GetComponent<RubyController>();
+
+        if (controller != null)
+        {
+            controller.ChangeHealth(-1);
+        }
+    }
+}
